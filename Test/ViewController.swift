7ebc670b33df1954c5b8fcc7c49ed16bbe0e80c1
Kenneth Kuo <kennethkuo1@gmail.com>
2015-10-17 12:43:25 -0400

    Everything

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -14,15 +14,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let view: UIView = NSBundle.mainBundle().loadNibNamed("InfoPage1For6and6P", owner: self, options: nil).last as! UIView
+        let view: UIView = NSBundle.mainBundle().loadNibNamed("ColorPicker5S", owner: self, options: nil).last as! UIView
         view.frame = self.view.frame
         self.view.addSubview(view)
     }
     
     //FacebookLogIn
-    //PersonaScreen6Plus
-    //PersonaScreen5Sand6
+    //PersonaScreen5S
+    //PersonaScreen6
+    //PersonaScreen6P
+    //PersonaScreen6PTest
     //InfoPage1For5S
+    //InfoPage1For6
     //InfoPage1For6and6P
     //OutfitPicker5S
     //OutfitPicker6
